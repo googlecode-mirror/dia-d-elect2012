2bad59c6a28f251f1b28afe3e8a51b78b4ece064
claudemirton <claudemirton@857948ec-4abe-d95f-abd4-f888236056ca>
2012-09-28 14:38:55 +0000

    retirada do captcha

--- trunk/elect2012/cadastro-advogado.php
+++ trunk/elect2012/cadastro-advogado.php
@@ -194,7 +194,7 @@
 												</div>										      
 											</div>
 											
-											<hr/>		    
+											<hr/><!--		    
 										    <div class="control-group">
 												<label class="control-label" for="codigo">* Código de verificação</label>
 												<div class="controls">													
@@ -207,7 +207,7 @@
 													</div>						        
 												</div>
 											</div>									
-								
+											-->
 							</fieldset>
 							<div class="form-actions">
 								<input type="hidden" name="acao" value="novo-adv"/>
